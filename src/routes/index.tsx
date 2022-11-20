4ad51080e3d650f581ec77c2348422dc7fb8471c
harshmangalam <harshdev8218@gmail.com>
2022-11-20 13:40:58 +0530

    code cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { Link } from "@builder.io/qwik-city";
-import { UsersIcon } from "~/icons/users";
 
 export default component$(() => {
-  return (
-    <div>
-      Hello Home
-      <Link href="/about">About</Link>
-      <Link href="/flower">Flower</Link>
-
-      <UsersIcon size={32} />
-    </div>
-  );
+  return <div>Home</div>;
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
