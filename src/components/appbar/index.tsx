d1dce4d215ef377ec77bca061a5b54a544b7e702
harshmangalam <harshdev8218@gmail.com>
2022-11-20 14:22:12 +0530

    added logo title

--- src/components/appbar/index.tsx
+++ src/components/appbar/index.tsx
@@ -1,13 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import { GithubIcon } from "~/icons/github";
-import { QwikLogo } from "~/icons/qwik";
+import { QwikIcon } from "~/icons/qwik";
 
 export const Appbar = component$(() => {
   return (
     <header class="bg-white border-b border-gray-300 shadow">
       <nav class="max-w-7xl px-4 mx-auto h-16 w-full flex items-center justify-between">
-        <div>
-          <QwikLogo />
+        <div class="flex items-center space-x-3">
+          <QwikIcon size={36} />
+          <h1 class="text-2xl ">Qwikhub</h1>
         </div>
 
         <ul>
