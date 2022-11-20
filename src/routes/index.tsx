17353d3a13d7f7ae30e7d6f2fbfd0e3e0f7b48d8
harshmangalam <harshdev8218@gmail.com>
2022-11-20 14:28:18 +0530

    added forms

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,26 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <div>Home</div>;
+  return (
+    <section class="max-w-md mx-auto">
+      <form method="POST" class="flex flex-col space-y-4">
+        <div class="flex flex-col space-y-2">
+          <label class="text-gray-600" for="username">Github username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="harshmangalam"
+          />
+        </div>
+
+        <button
+          type="submit"
+          class="bg-blue-500 text-white font-medium py-3 px-4"
+        >
+          Continue
+        </button>
+      </form>
+    </section>
+  );
 });
