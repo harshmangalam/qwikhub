9ed339e9c29ed9850bd9e77b899a83c71e5752de
harshmangalam <harshdev8218@gmail.com>
2022-11-20 14:58:45 +0530

    added validation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,44 @@
-import { component$ } from "@builder.io/qwik";
-import { RequestHandler } from "@builder.io/qwik-city";
+import { component$, Resource } from "@builder.io/qwik";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const endpointData = useEndpoint<ReturnType<typeof onPost>>();
   return (
     <section class="max-w-md mx-auto">
-      <form method="POST" class="flex flex-col space-y-4">
-        <div class="flex flex-col space-y-2">
-          <label class="text-gray-600" for="username">
-            Github username
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="harshmangalam"
-          />
-        </div>
+      <Resource
+        value={endpointData}
+        onPending={() => <div>Loading...</div>}
+        onRejected={() => <div>Error</div>}
+        onResolved={(data) => (
+          <form method="POST" class="flex flex-col space-y-4">
+            <div class="flex flex-col space-y-2">
+              <label
+                class={`text-gray-600 ${data?.error?.username ? "text-red-500 " : ""}`}
+                for="username"
+              >
+                Github username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="harshmangalam"
+                class={`${data?.error?.username ? "border-red-500" : ""}`}
+              />
+              {data?.error && (
+                <p class="text-red-500 text-sm ">{data.error.username}</p>
+              )}
+            </div>
 
-        <button
-          type="submit"
-          class="bg-blue-500 text-white font-medium py-3 px-4"
-        >
-          Continue
-        </button>
-      </form>
+            <button
+              type="submit"
+              class="bg-blue-500 text-white font-medium py-3 px-4"
+            >
+              Continue
+            </button>
+          </form>
+        )}
+      />
     </section>
   );
 });
@@ -32,6 +47,7 @@ export const onPost: RequestHandler = async ({ request }) => {
   const formData = await request.formData();
   const username = formData.get("username");
 
+  console.log("username:-", username);
   if (!username || username.toString().trim().length === 0) {
     return {
       error: {
@@ -39,4 +55,10 @@ export const onPost: RequestHandler = async ({ request }) => {
       },
     };
   }
+
+  return {
+    input: {
+      username,
+    },
+  };
 };
